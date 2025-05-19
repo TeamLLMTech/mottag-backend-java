f93883596a300b45b60a856bcbb8a0160719c1f5
Ribaas <ribasleonardo92@gmail.com>
2025-05-19 17:55:25 -0300

    fix: adicionando annotation Entity

--- api/src/main/java/br/com/mottag/api/model/Patio.java
+++ api/src/main/java/br/com/mottag/api/model/Patio.java
@@ -1,9 +1,11 @@
 package br.com.mottag.api.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Patio {
 
     @Id
