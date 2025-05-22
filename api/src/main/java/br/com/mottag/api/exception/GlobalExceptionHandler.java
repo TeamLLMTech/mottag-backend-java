8ccbd2f8ee4d25689c43ba2e033869114d56467c
Ribaas <ribasleonardo92@gmail.com>
2025-05-22 15:38:50 -0300

    feat: melhorando exceptions

--- api/src/main/java/br/com/mottag/api/exception/GlobalExceptionHandler.java
+++ api/src/main/java/br/com/mottag/api/exception/GlobalExceptionHandler.java
@@ -3,12 +3,14 @@ package br.com.mottag.api.exception;
 import br.com.mottag.api.dto.common.ApiErrorDTO;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.HashMap;
@@ -53,10 +55,29 @@ public class GlobalExceptionHandler {
         }
     }
 
+    @ExceptionHandler(PropertyReferenceException.class)
+    public ResponseEntity<ApiErrorDTO> handlePropertyReferenceException(PropertyReferenceException ex) {
+        ApiErrorDTO error = new ApiErrorDTO(ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ApiErrorDTO> handlePropertyReferenceException(MethodArgumentTypeMismatchException ex) {
+        ApiErrorDTO error = new ApiErrorDTO(ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
     public ResponseEntity<ApiErrorDTO> handleSQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException ex) {
         ApiErrorDTO error = new ApiErrorDTO("Não foi possível concluir a operação. Os dados informados violam uma restrição de integridade no banco de dados.");
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    // Catch all other exceptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorDTO> handleAllOtherExceptions(Exception ex) {
+        ApiErrorDTO error = new ApiErrorDTO("Erro no servidor");
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
